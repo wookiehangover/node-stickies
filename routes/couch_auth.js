1c38e223ee107c839e4c81cbd0fd13928ac936de
Sam Breed <sam@quickleft.com>
2013-11-11 23:19:30 -0700

    refactoring auth endpoint with deferreds

--- routes/couch_auth.js
+++ routes/couch_auth.js
@@ -1,14 +1,20 @@
+var Q = require('q');
+var config = require('config');
 var userValidate = require('npm-user-validate');
+var CouchLogin = require('couch-login');
+
 
 function formatName(name){
   return name.toLowerCase().replace(/@/, '-at-').replace(/\./g,'-dot-');
 }
 
 module.exports = function(app){
+  var basicCouchLogin = new CouchLogin(config.COUCH_URL, 'basic');
 
   app.post('/auth', function(req, res){
     var errors = [];
     var user = req.body.auth;
+    var name = formatName(req.body.auth.name);
 
     userValidate.email(user.name) && errors.push(userValidate.email(user.name));
     userValidate.pw(user.password) && errors.push(userValidate.pw(user.password));
@@ -17,43 +23,47 @@ module.exports = function(app){
       return res.json(500, { error: errors });
     }
 
-    req.couch.signup(user, function(err, resp, body){
-      if( err ){
-        return res.json(500, { error: err });
+    var response = {
+      email: req.body.auth.name,
+      database: name
+    };
+
+    Q.nfcall(req.nano.request, {
+      path: '_users/org.couchdb.user:'+user.name,
+    })
+    .then(function(resp, body){
+      // Log user in if account exitsts
+      return Q.ninvoke(res.couch, 'login', user);
+    }, function(err, resp){
+      if( err.status_code !== 404 ){
+        console.log(resp);
+        console.error('unexpected error with user lookup', err);
+        throw new Error(err);
       }
 
-      var name = formatName(req.body.auth.name);
-
-      var response = {
-        email: req.body.auth.name,
-        database: name,
-      };
-
-      var onSuccess = function(err, resp, body){
-        if( err){
-          return res.json(500, { error: err || body });
-        }
-
-        if( res.couch.token ){
-          response.cookie = res.couch.token;
-        }
-
-        if( body && body.error ){
-          return res.json(resp.statusCode, body);
-        }
-
-        res.json(response);
-      };
-
-      console.log(resp.statusCode, body)
-
-      if( resp.statusCode === 409 ){
-        res.couch.login(user, onSuccess);
-      } else {
-        req.nano.db.create(name + '_stickies', onSuccess);
+      // Sign up User
+      return Q.ninvoke(basicCouchLogin, 'signup', user).then(function(resp, body){
+        // console.log('signup succesful', body, res.couch);
+        return Q.ninvoke(res.couch, 'login', user).then(function(resp, body){
+          // console.log('sign in successful', body, res.couch);
+          return Q.nfcall(req.nano.db.create, name +'_stickies');
+        });
+      });
+    })
+    .then(function(resp, body){
+      if( res.couch.token ){
+        response.cookie = res.couch.token;
       }
-
+      if( body && body.error ){
+        return res.json(resp.statusCode, body);
+      }
+      return res.json(response);
+    })
+    .fail(function(err){
+      console.error(err);
+      return res.json(500, { error: err });
     });
+
   });
 
 };
